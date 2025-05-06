09495f137f8033684193d9a5aa0169a64cd7a3e3
henyiwow <ken731083@gmail.com>
2025-05-06 14:24:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def fetch_news():
         "https://news.google.com/rss/search?q=新光金控+OR+新光人壽+OR+保險+OR+金控+OR+人壽&hl=zh-TW&gl=TW&ceid=TW:zh-Hant",  # Google News
         "https://www.ettoday.net/news/rss",  # ETtoday
         "https://www.chinatimes.com/rss",  # 中時新聞
-        # "https://www.ltn.com.tw/rss",  # 自由時報
+        "https://news.ltn.com.tw/rss",  # 自由時報
         "https://www.setn.com/RSS.aspx",  # 三立新聞
         "https://www.cna.com.tw/rss",  # 中央社
         "https://www.udn.com/rssfeed/news/2",  # 經濟日報
