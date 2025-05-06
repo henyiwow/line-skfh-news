c8fd28c4780f282ee6d0d84915cdb2bb76cd7f18
henyiwow <ken731083@gmail.com>
2025-05-06 14:45:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,9 +46,9 @@ def classify_news(title):
 def fetch_news():
     rss_urls = [
         "https://news.google.com/rss/search?q=新光金控+OR+新光人壽+OR+保險+OR+金控+OR+人壽&hl=zh-TW&gl=TW&ceid=TW:zh-Hant",  # Google News
-        # "https://www.ettoday.net/news/rss",  # ETtoday
-        # "https://www.chinatimes.com/rss",  # 中時新聞
-        "https://news.ltn.com.tw/rss/all.xml",  # 自由時報
+        "https://www.ettoday.net/news",  # ETtoday
+        "https://www.chinatimes.com/rss/realtimenews",  # 中時新聞
+        "https://news.ltn.com.tw/rss/business.xml",  # 自由時報
         # "https://www.setn.com/RSS.aspx",  # 三立新聞
         # "https://www.cna.com.tw/rss",  # 中央社
         # "https://www.udn.com/rssfeed/news/2",  # 經濟日報
@@ -125,7 +125,7 @@ def broadcast_message(message):
 if __name__ == "__main__":
     news = fetch_news()
     if news:
-        broadcast_message("【業務企劃部 今日新光保險新聞】\n\n" + news)
+        broadcast_message("【業企部 今日重點新聞整理】\n\n" + news)
     else:
         print("⚠️ 沒有符合條件的新聞，不發送。")
 
