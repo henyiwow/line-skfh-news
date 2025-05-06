511ad713ec4adda5997aaeeb1a0a8ca0ebc215db
henyiwow <ken731083@gmail.com>
2025-05-06 14:53:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,12 +46,12 @@ def classify_news(title):
 def fetch_news():
     rss_urls = [
         "https://news.google.com/rss/search?q=新光金控+OR+新光人壽+OR+保險+OR+金控+OR+人壽&hl=zh-TW&gl=TW&ceid=TW:zh-Hant",  # Google News
-        "https://www.ettoday.net/news/news-feed.aspx?rss=1",  # ETtoday
-        # "https://www.chinatimes.com/rss/realtimenews",  # 中時新聞
+        "https://www.ettoday.net/news/focus/2000000002/rss.xml",  # ETtoday
         "https://news.ltn.com.tw/rss/business.xml",  # 自由時報
-        # "https://www.setn.com/RSS.aspx",  # 三立新聞
-        # "https://www.cna.com.tw/rss",  # 中央社
-        # "https://www.udn.com/rssfeed/news/2",  # 經濟日報
+        "https://www.chinatimes.com/rss/realtimenews",  # 中時新聞
+        "https://www.setn.com/RSS.aspx",  # 三立新聞
+        "https://www.cna.com.tw/rss",  # 中央社
+        "https://www.udn.com/rssfeed/news/2",  # 經濟日報
     ]
     
     classified_news = {cat: [] for cat in CATEGORY_KEYWORDS}
